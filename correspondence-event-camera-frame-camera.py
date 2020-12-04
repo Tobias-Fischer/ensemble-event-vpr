3a45fb45e27aa0db513ab0e86f9addb5954a8f13
Tobias Fischer <info@tobiasfischer.info>
2020-12-04 12:58:37 +1000

    Create correspondence-event-camera-frame-camera.py

---
+++ correspondence-event-camera-frame-camera.py
@@ -0,0 +1,26 @@
+traverse_to_name = {
+    'run_tobi3_dvs_vpr_2020-04-21-17-03-03': 'sunset1',
+    'run_tobi3_dvs_vpr_2020-04-22-17-24-21': 'sunset2',
+    'run_tobi3_dvs_vpr_2020-04-24-15-12-03': 'daytime',
+    'run_tobi3_dvs_vpr_2020-04-28-09-14-11': 'morning',
+    'run_tobi3_dvs_vpr_2020-04-29-06-20-23': 'sunrise',
+    'run_tobi3_dvs_vpr_2020-04-27-18-13-29': 'night'
+}
+
+name_to_consumervideo = {
+    'sunset1': '20200421_170039-sunset1',
+    'sunset2': '20200422_172431-sunset2',
+    'daytime': '20200424_151015-daytime',
+    'night': '20200427_181204-night',
+    'morning': '20200428_091154-morning',
+    'sunrise': '20200429_061912-sunrise'
+}
+
+video_beginning = {
+    'sunset1': 1587452582.35,
+    'sunset2': 1587540271.65,
+    'daytime': 1587705130.80,
+    'morning': 1588029265.73,
+    'sunrise': 1588105232.91,
+    'night': 1587975221.10
+}
